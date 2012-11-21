91097f91d13249814fe8cc44a297552ccbfb66de
unknown <Philip.Stahl@vision.iig.uni-freiburg.de>
2012-11-21 12:43:38 +0100

    added activation functions for cogmodule

--- src/cogmodule.py
+++ src/cogmodule.py
@@ -82,7 +82,8 @@ class CogModule:
         summed = 0
         now = self.seconds(datetime.datetime.now())
         for j in range(n):
-            summed += math.pow(now - times[j], -d)
+            diff = now - times[j]
+            summed += math.pow(diff, -d)
 
         B = math.log(summed)
         return B
@@ -156,6 +157,235 @@ class CogModule:
                 return self.get_emotion_by_name(CogModule.EXPECT_POS[2])
 
 
+    def get_remaining_task(self, task, current):
+        # remove all items which are already done:
+        remaining_task = []
+        i = 0
+        for group in task:
+            remaining_group = []
+            for item in group:
+                if i < current:
+                    i += 1
+                else:
+                    remaining_group.append(item)
+            if remaining_group:
+                remaining_task.append(remaining_group)
+
+        return remaining_task
+
+    def focus_activation(self, task, current):
+        ''' The activation of a chunk is the sum of its base-level activation
+            and the activations it receives from the elements currently in
+            the focus of attention. Formally, the equation in ACT-R for the
+            actvation, Ai , of chunk i is
+
+            Ai = Bi + sum_{over j} Wj * Sji
+
+            where Bi is the base-level activation of chunk i, Wj is the
+            salience or source activation of element j in the focus of
+            attention, and Sji is the strength of association from element j
+            to chunk i.
+
+            FRAGE: WELCHE EMLEMENTE SIND ALLES IM FOKUS?
+        '''
+
+        
+        ''' The typical ACT-R assumption is that, if there are n sources of
+            activation the Wj are all set to l/n.
+        '''
+
+        #Wj = 1/n
+
+        ''' The associative strengths, Sji, reflect the competition among
+            the associations to the cue j. According to the ACT-R theory,
+            Sji = S + ln(P(i | j)), where P(i | j) is the probability that
+            chunk i will be needed when j appears in the context.
+            S is a constant.
+
+            That is, if there are m elements associated to j their average
+            probability will be l/m and Sji = S - ln(m). This is the
+            simplification that will be used in all the simulations presented
+            in the paper. Thus,
+
+            Sji = S - ln(m).
+
+            m = NR ELEMENTS ASSOCIATED TO j
+
+            S = constant: WELCHER WERT?
+                It can be set in the simulation but if not set it will default to the log
+                of the total number of chunks.
+        '''
+        n = 0                                   # total nr of items
+        for group in task:
+            n += len(group)
+            
+        nr_chunks = len(task)                   # total nr of chunks
+
+        # remove all items which are already done:
+        remaining_task = []
+        i = 0
+        for group in task:
+            remaining_group = []
+            for item in group:
+                if i < current:
+                    i += 1
+                else:
+                    remaining_group.append(item)
+            if remaining_group:
+                remaining_task.append(remaining_group)
+
+        print 'remaining task:', remaining_task
+        task = remaining_task
+
+        activations = []
+        for group in task:
+            group_activations = []
+            for item in group:
+                group_activations.append(0.0)
+            activations.append(group_activations)
+
+        nr_elem_associated_to = []
+        for group in task:
+            #nr_elem_associated_to.append(len(group))
+            nr_elem_associated_to.append(len(group) - 1)
+
+        for group_index in range(len(activations)):
+
+
+            for i in range(len(activations[group_index])):
+
+
+                
+                m = nr_elem_associated_to[group_index]      # nr elements associated to j
+                if m == 0:
+                    m = 1
+                
+                S = math.log(nr_chunks)
+            
+                elements_in_focus = 0                       
+                #for group in task:                                  # ALLE ELEMENTE
+                #    elements_in_focus += len(group)
+                elements_in_focus = len(activations[group_index])    # NUR DIE DER GRUPPE?
+            
+
+                for j in range(elements_in_focus):
+                    Wj = 1.0 / n
+                    Sji = S - math.log(m)
+
+
+
+                    add = Wj * Sji
+                    #print '+= ', add
+                    activations[group_index][i] += add
+
+        return activations
+
+    def probability_of_retrieval(self):
+        ''' 1 / (1 + e_to_the_power(Ai - threshold))
+        '''
+        pass
+
+    def equation8(self, time, length):
+        ''' Activation = ln(n) - 0.5 * ln(T) - ln(L)
+
+            n = number of rehearsals
+            T = delay / time since presentation
+            L = list length
+        '''
+        activation = math.log(1.0) - 0.5 * math.log(time) - math.log(length)
+        return activation
+            
+
 if __name__ == '__main__':
     cog = CogModule()
-    cog.print_random_activation_values(10, 20)
+    #cog.print_random_activation_values(10, 20)
+
+    print 'Cog module started:'
+
+    task = [[1, 5, 3], [8, 4, 7], [2, 6, 9]]
+    length = len(task)
+    current = 0
+
+    focus_activations = cog.focus_activation(task, current)
+
+    print 'focus:', focus_activations
+    
+    now = cog.seconds(datetime.datetime.now())
+    diff = 9
+    times = []
+    for d in range(diff):
+        times.insert(0, now - (d+2))
+
+    print task
+    print times
+
+    task = cog.get_remaining_task(task, current)
+    print task
+
+    num_remaining = 0
+    for group in task:
+        num_remaining += len(group)
+    times = times[(len(times) - num_remaining):]
+
+    time_diffs = []
+    for time in times:
+        time_diffs.append(now - time)
+        
+    print 'times:', times
+    print 'diffs:', time_diffs
+    
+    baselevel_activations = []
+    for group_index in range(len(task)):
+        group_activations = []
+        for item_index in range(len(task[group_index])):
+            index = item_index
+            if group_index == 1:
+                index += len(task[group_index-1])
+            if group_index == 2:
+                index += len(task[group_index-1]) + len(task[group_index-2])
+            
+            time = [times[index]]
+
+            activation = cog.baselevel_activation(time, 0.5)
+            group_activations.append(activation)
+        baselevel_activations.append(group_activations)
+            
+    print 'baselevel:', baselevel_activations
+
+    activations = []
+    for group_index in range(len(baselevel_activations)):
+        group_activations = []
+        for item_index in range(len(baselevel_activations[group_index])):
+            activation = baselevel_activations[group_index][item_index] + focus_activations[group_index][item_index]
+            group_activations.append(activation)
+        activations.append(group_activations)
+
+    print 'ACTIVATIONS:'
+    for group in activations:
+        for item in group:
+            print item
+
+
+    '''EQUATION 8:'''
+    activations = []
+    for group_index in range(len(task)):
+        group_activations = []
+        for item_index in range(len(task[group_index])):
+            index = item_index
+            if group_index == 1:
+                index += len(task[group_index-1])
+            if group_index == 2:
+                index += len(task[group_index-1]) + len(task[group_index-2])
+            
+            time = time_diffs[index]
+
+            activation = cog.equation8(time, length)
+            group_activations.append(activation)
+        activations.append(group_activations)
+
+    print 'EQUATION 8'
+    for group in activations:
+        for item in group:
+            print item
+
+    
